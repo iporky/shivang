e1cedf28355d75e07b00cc566a5c8cfa0c14adf9
iporky <chauhanshivang4@gmail.com>
2016-02-18 19:20:29 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Comment Box</title>
+        <link href="httpdocs/css/layout.css" rel="stylesheet" />
+        <script type="text/javascript" src="httpdocs/js/jquery.js"> </script>
+        <script type="text/javascript" src="httpdocs/js/script.js"> </script>
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="page-data">
+                page data is in here
+            </div>
+            <div class="comment-wrapper">
+                <h3 class="comment-title">User comments.....</h3>
+                <div class="comments-list">
+                    
+                    <ul class="comment-holder-ul">
+                        <li class="comment-holder" id="_1">
+                            <div class="usr-img">
+                                <img src="httpdocs/images/saitama.jpg" class="usr-img-pic" />
+                            </div> 
+                             <div class="comment-body">
+                                <h3 class="usrnm">
+                                   Shivang Chauhan
+                                </h3>
+                                <div class="comment-text">
+                                asdsadsada asdsadsadaasd sadsadaasds adsadaasdsadsad aasdsadsada asdsadsada asdsadsada asdsadsada asdsadsada
+                                     
+                                    
+                                </div>
+                             </div>   
+                            
+                            <div class="comment-button-hldr">
+                                <ul>
+                                    <li class="dlt-btn">
+                                    X
+                                    </li>
+                                
+                                </ul> 
+                            
+                            </div>
+                        
+                        </li>
+                        
+                        
+                        
+                        
+                        <li class="comment-holder" id="_1">
+                            <div class="usr-img">
+                                <img src="httpdocs/images/saitama.jpg" class="usr-img-pic" />
+                            </div> 
+                             <div class="comment-body">
+                                <h3 class="usrnm">
+                                   Shivang Chauhan
+                                </h3>
+                                <div class="comment-text">
+                                
+                                      Comments.......
+                                </div>
+                             </div>   
+                            
+                            <div class="comment-button-hldr">
+                                <ul>
+                                    <li class="dlt-btn">
+                                    X
+                                    </li>
+                                
+                                </ul> 
+                            
+                            </div>
+                        </li>
+                        
+                        
+                    </ul>
+                    
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
