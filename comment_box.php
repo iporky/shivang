d9c557d808c785654eda65e9868b045cb9e99573
iporky <chauhanshivang4@gmail.com>
2016-02-19 19:19:20 +0530

    Create comment_box.php

---
+++ comment_box.php
@@ -0,0 +1,31 @@
+<?php foreach($comments as $key=> $comment):?>
+
+
+
+<li class="comment-holder" id="_1">
+                            <div class="usr-img">
+                                <img src="httpdocs/images/saitama.jpg" class="usr-img-pic" />
+                            </div> 
+                             <div class="comment-body">
+                                <h3 class="usrnm">
+                                   Saitama
+                                </h3>
+                                <div class="comment-text">
+                                asdsadsada asdsadsadaasd sadsadaasds adsadaasdsadsad aasdsadsada asdsadsada asdsadsada asdsadsada asdsadsada
+                                     
+                                    
+                                </div>
+                             </div>   
+                            
+                            <div class="comment-button-hldr">
+                                <ul>
+                                    <li class="dlt-btn">
+                                    X
+                                    </li>
+                                
+                                </ul> 
+                            
+                            </div>
+                        
+                        </li>
+                        <?php endforeach; ?>
