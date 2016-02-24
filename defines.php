f2e4ad128e57d8e54be84bc471077a27e6dabe9d
iporky <chauhanshivang4@gmail.com>
2016-02-24 19:17:25 +0530

    the commentBox

--- defines.php
+++ defines.php
@@ -2,5 +2,9 @@
 define('DOC_ROOT' , $_SERVER['DOCUMENT_ROOT'].'/');
 define('DS', DIRECTORY_SEPARATOR);
 
-define('INCLUDES', DOC_ROOT . 'PhpProject1/httpdocs/includes'. DS)
+define('INCLUDES', DOC_ROOT . 'PhpProject1/httpdocs/includes'. DS);
+define('MODELS_DIR', DOC_ROOT . 'PhpProject1/httpdocs/mySql'.DS.'models'.DS);
+define('MYSQL_DIR', DOC_ROOT . 'PhpProject1/httpdocs/mySql'.DS);
+
+require_once MYSQL_DIR.'db_connect.php';
 ?>
